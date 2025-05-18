95d28318b2613154fa481c340e086753c06129b1
Uvindu Sankalpa <uvindusankalpa2021@gmail.com>
2025-05-18 23:15:39 +0530

    git init

---
+++ README.md
@@ -0,0 +1,15 @@
+# React + Vite + Electron
+
+npm create vite@latest
+
+cd electron-with-react
+
+npm install
+
+npm install --save-dev concurrently
+
+npm install --save-dev cross-env
+
+npm install --save-dev wait-on
+
+npm run electron:serve => run cmd
---
+++ public/electron.cjs
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:5173");
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
